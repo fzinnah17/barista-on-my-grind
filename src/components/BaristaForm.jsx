b8a318e935607b8457f4035aae768af7408c82d1
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-03-07 20:22:03 -0500

    Add state management and integrate RecipeChoices component

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
+import RecipeChoices from "./RecipeChoices";
 
 const BaristaForm = () => {
-  const onNewDrink = () => {};
-  const onCheckAnswer = () => {};
+  const [inputs, setInputs] = useState({
+    temperature: "",
+    milk: "",
+    syrup: "",
+    blended: "",
+  });
+
+  const ingredients = {
+    temperature: ["hot", "lukewarm", "cold"],
+    syrup: ["mocha", "vanilla", "toffee", "maple", "caramel", "other", "none"],
+    milk: ["cow", "oat", "goat", "almond", "none"],
+    blended: ["yes", "turbo", "no"],
+  };
 
   return (
     <div>
       <h2>Hi, I'd like to order a:</h2>
       <form>
-        <button type="submit" className="button submit" onClick={onCheckAnswer}>
+        {Object.keys(ingredients).map((category) => (
+          <div key={category}>
+            <h3>{category}</h3>
+            <div className="answer-space">{inputs[category]}</div>
+            <RecipeChoices
+              handleChange={(e) =>
+                setInputs((prevState) => ({
+                  ...prevState,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+              label={category}
+              choices={ingredients[category]}
+              checked={inputs[category]}
+            />
+          </div>
+        ))}
+        <button type="submit" className="button submit">
           Check Answer
         </button>
-        <button type="button" className="button new-drink-button" onClick={onNewDrink}>
+        <button type="button" className="button new-drink-button">
           New Drink
         </button>
       </form>
